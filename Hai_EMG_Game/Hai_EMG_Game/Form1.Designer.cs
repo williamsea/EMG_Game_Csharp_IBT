19e7f6546108ba77f3d4e10a1448a2e1334212b9
williamsea <william.sea@jhu.edu>
2015-08-19 11:08:37 -0400

    Project upload

---
+++ Hai_EMG_Game/Hai_EMG_Game/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Hai_EMG_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.button_start = new System.Windows.Forms.Button();
+            this.timer_display = new System.Windows.Forms.Timer(this.components);
+            this.chart_EMGrealtime = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart_DigitBar = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.timerTest = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart_EMGrealtime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_DigitBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.BaudRate = 250000;
+            this.serialPort.PortName = "COM2";
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 348);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // timer_display
+            // 
+            this.timer_display.Interval = 1;
+            this.timer_display.Tick += new System.EventHandler(this.timer_display_Tick);
+            // 
+            // chart_EMGrealtime
+            // 
+            chartArea1.BorderWidth = 2;
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart_EMGrealtime.ChartAreas.Add(chartArea1);
+            this.chart_EMGrealtime.Location = new System.Drawing.Point(252, 3);
+            this.chart_EMGrealtime.Name = "chart_EMGrealtime";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "EMGVal";
+            this.chart_EMGrealtime.Series.Add(series1);
+            this.chart_EMGrealtime.Size = new System.Drawing.Size(825, 725);
+            this.chart_EMGrealtime.TabIndex = 3;
+            this.chart_EMGrealtime.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "EMG Envelop";
+            title1.Text = "EMG Envelop";
+            this.chart_EMGrealtime.Titles.Add(title1);
+            // 
+            // chart_DigitBar
+            // 
+            chartArea2.AxisY.Interval = 10D;
+            chartArea2.AxisY.Maximum = 80D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.BorderWidth = 2;
+            chartArea2.CursorX.IsUserSelectionEnabled = true;
+            chartArea2.Name = "ChartArea1";
+            this.chart_DigitBar.ChartAreas.Add(chartArea2);
+            this.chart_DigitBar.Location = new System.Drawing.Point(1083, 3);
+            this.chart_DigitBar.Name = "chart_DigitBar";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "BarEMGVal";
+            series2.YValuesPerPoint = 2;
+            this.chart_DigitBar.Series.Add(series2);
+            this.chart_DigitBar.Size = new System.Drawing.Size(328, 725);
+            this.chart_DigitBar.TabIndex = 5;
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.Name = "Real Time Bar";
+            title2.Text = "Real Time Bar";
+            this.chart_DigitBar.Titles.Add(title2);
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(154, 348);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 6;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // timerTest
+            // 
+            this.timerTest.Interval = 1;
+            this.timerTest.Tick += new System.EventHandler(this.timerTest_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1474, 740);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.chart_DigitBar);
+            this.Controls.Add(this.chart_EMGrealtime);
+            this.Controls.Add(this.button_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart_EMGrealtime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_DigitBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Timer timer_display;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_EMGrealtime;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_DigitBar;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.Timer timerTest;
+    }
+}
+
---
+++ Hai_EMG_Game/Hai_EMG_Game/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*
+Author: Hai Tang (haitang@jhu.edu)
+*/
+namespace Hai_EMG_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Data Acquisition and Bit Manipulation
+        int counter = 0;
+        int[] receivedBuffer = new int[500];
+        int firstByte;
+        int secondByte;
+        int thirdByte;
+        int combine;
+        bool sign = false;
+        int[] envelop = new int[1000000];//1000s
+        int[] DACenvelop = new int[1000000];
+        int[] digitizedEnvelop = new int[1000000];
+        int signalPeak = 800;
+        double stepSize = 255 / 77; //0-255, digitizedLevel = 77;
+        Encoding enc = Encoding.GetEncoding(1252);
+
+        //Display
+        int DisplayLength = 10000; //Sampling rate = 1000
+        int disp;
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            for (int i = 0; i < receivedBuffer.Length; i++)
+            {
+                receivedBuffer[i] = serialPort.ReadByte();
+                //Read a byte from the stream and advances the position within the stream by one byte, or returns -1 if at the end of the stream.
+            }
+
+            for (int i = 0; i <= receivedBuffer.Length - 5; i++)
+            {
+                //refresh the buffer and reset counter when it's full
+                if (counter == 1000000) 
+                {
+                    Array.Clear(envelop, 0, envelop.Length);
+                    Array.Clear(digitizedEnvelop, 0, digitizedEnvelop.Length);
+                    counter = 0;
+                }
+
+                if (receivedBuffer[i] == 35 && receivedBuffer[i + 4] == 36)
+                {
+                    //NOTE: Least significant bit comes first!
+                    firstByte = receivedBuffer[i + 1];
+                    secondByte = receivedBuffer[i + 2];
+                    thirdByte = receivedBuffer[i + 3];
+
+                    combine = firstByte << 16 | secondByte << 8 | thirdByte; //Concat three bytes together bitwisely
+                    sign = GetBit(firstByte, 7); //When the sign bit is 0 (false), positive; 1 (true), negative
+                    if (!sign) //Positive number
+                    {
+                        envelop[counter] = combine;
+                    }
+                    else //Negative number
+                    {
+                        //combine = (~firstByte) << 16 | (~secondByte) << 8 | (~thirdByte); //Take the complement. ~ is complement!
+                        //Pad the extra leading byte for int with 11111111. Which is the "sign byte"
+                        for (int temp = 24; temp < 32; temp++)
+                        {
+                            combine = combine | (1 << temp);
+                        }
+                        envelop[counter] = combine; //Take the correct negative value. Not need to take complement and plus 1 any more.
+                    }
+                    DACenvelop[counter] = envelop[counter] * 255 / signalPeak;
+                    if (DACenvelop[counter] > 255)
+                    {
+                        DACenvelop[counter] = 255;
+                    }
+                    digitizedEnvelop[counter] = (int)(DACenvelop[counter] / stepSize); //No need to floor an int since it's auto truncked.
+                    counter++;
+                }
+            }
+
+            System.Threading.Thread.Sleep(10);//Slightly delay for 100; 50 works fine; 30 works fine. 10 works fine. 
+        }
+
+        private static bool GetBit(int b, int bitNum) // a=11010010, GetBit(a,0) = 0; GetBit(a,7) = 1;
+        {
+            return (b & (1 << bitNum)) != 0;
+        }
+
+        private Byte ReverseBits(Byte originalByte)
+        {
+            int result = 0;
+            for(int i=0; i<8; i++)
+            {
+                result = result << 1;
+                result += originalByte & 1;
+                originalByte = (Byte)(originalByte >> 1);
+            }
+            return (Byte)result;
+        }
+
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open(); 
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Cannot Open Serial Port. Check COM Number or Availability.");
+            }
+
+            timer_display.Enabled = true;
+        }
+
+        private void timer_display_Tick(object sender, EventArgs e)
+        {
+            this.Invoke(new EventHandler(DisplayData));
+        }
+
+        private void DisplayData(object s, EventArgs e)
+        {
+
+            if (counter > 1)
+            {
+                this.chart_DigitBar.Series["BarEMGVal"].Points.Clear();
+                this.chart_DigitBar.Series["BarEMGVal"].Points.AddXY("Strength", digitizedEnvelop[counter-1]); //Note that counter++ after putting in data. So we need counter - 1 here!!!
+            }
+
+
+            this.chart_EMGrealtime.Series["EMGVal"].Points.Clear();
+            if (counter >= DisplayLength)
+            {
+                for (disp = counter - DisplayLength; disp < counter; disp++)
+                {
+                    this.chart_EMGrealtime.Series["EMGVal"].Points.AddXY((disp / 1000).ToString(), envelop[disp]);
+                }
+            }
+            else
+            {
+                for (disp = 0; disp < DisplayLength; disp++)
+                {
+                    this.chart_EMGrealtime.Series["EMGVal"].Points.AddXY((disp / 1000).ToString(), envelop[disp]);
+                }
+            }
+        }
+
+
+
+        //Test button and timer for debugging
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            this.chart_DigitBar.Series["BarEMGVal"].Points.Clear();
+            this.chart_DigitBar.Series["BarEMGVal"].Points.AddXY("Halo", 100);
+            timerTest.Enabled = true;
+        }
+
+        private void timerTest_Tick(object sender, EventArgs e)
+        {
+            disp++;
+            this.chart_DigitBar.Series["BarEMGVal"].Points.Clear();
+            this.chart_DigitBar.Series["BarEMGVal"].Points.AddXY("Halo", disp);
+        }
+    }
+}
